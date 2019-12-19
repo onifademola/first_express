84bba3afa5021d7512ac187d057f3d05784d06e5
Onifade Demola <devep.aonifade@gmail.com>
2019-12-19 07:01:41 +0100

    change 2

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const Joi = require('joi');
+const morgan = require('morgan');
 const express = require ('express');
 const logger = require('./logger');
 const app = express();
+
+console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+console.log(`App: ${app.get('env')}`);
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.use(logger);
+
+if(app.get('env') === 'development') {
+    app.use(morgan('tiny'));
+    console.log('Morgan is enabled...')
+}
 
 app.get('/', (req, res) =>{
     res.send('Hello Heavens oo!!')
